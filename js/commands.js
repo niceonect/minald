4902e5d26ef0a914f4a041f182ed31ca45195ccc
niceone <94924743+niceonect@users.noreply.github.com>
2024-10-23 15:59:54 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,71 @@
+var pumpfun = "https://pump.fun/6nR7QvtpWfhcM5MCcE3MqjXSJWTW2ErRYQL9TCbcoPFZ";
+var twitter = "https://x.com/cto_terminal";
+var telegram = "https://www.linkedin.com/in/prithvi-yewale-a77275228/";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm prithvi!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently a 3rd year undergrad, pursuing BSc.</li>",
+  "<li>🌱 My fields of intetest are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development
+        - UI/UX Designing</pre>`,
+  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
+   professional website for their business or even a personal 
+   website to showcase their portfolio.
+   I love learning new stuff and using it to improve my skills.
+   I primarily use NextJS and Tailwind CSS when building my 
+   websites (projects).
+   You can learn more about the projects that I have built using 
+   the project command.
+   You can browse around my website to find out more about 
+   me.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=yewaleprithvi2003@gmail.com">yewaleprithvi2003@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'pumpfun        <a href="' +
+    pumpfun +
+    '" target="blank">https://pump.fun/6nR7QvtpWfhcM5MCcE3MqjXSJWTW2ErRYQL9TCbcoPFZ' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">https://x.com/cto_terminal' +
+    "</a>",
+  'telegram       <a href="' +
+    telegram +
+    '" target="blank">t.me/terminalcto' +
+    "</a>",
+ 
+  
+  "<br>",
+];
+
+
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays about terminalcto?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome terminal_01_cto.exe! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
